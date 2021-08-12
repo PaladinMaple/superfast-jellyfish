619c8ffeb3d7d3c6fc00ec4471f9b338d690d12c
Paladin Maple <karina@quadrosoluciones.mx>
2021-08-12 13:43:06 -0500

    Leccion 5 eventos

--- Leccion1/index.js
+++ Leccion1/index.js
@@ -1,16 +1,23 @@
-// let title = 'Hola!'
-
-// const h1 = document.getElementById('h1')
-// h1.innerText = title
-
-// title = 'Chau!'
-// h1.innerText = title
-
 const app = new Vue({
-    el: '#app',    
-    data () {
-      return {
-        title: 'Hola Vue!'
-      }
-    }
+  el: '#app',    
+  data:{
+    img: 'https://pbs.twimg.com/profile_images/623323276522188801/D1ehYWlt.jpg',
+    name: 'Duck Test',
+    //Note: Ducks live between 5 and 10 years
+    duckAge: 12,
+    ducksNames: ['Mariano', 'Jeremy', 'Juan', 'Carlos', 'Cesar'],
+    ducksDetail: [
+      { name: 'Mariano', age: 8 },
+      { name: 'Jeremy', age: 7 },
+      { name: 'Juan', age: 2 },
+      { name: 'Carlos', age: 9 },
+      { name: 'Cesar', age: 4 }
+  ],
+  showDucksData: false
+},
+methods: {
+  changeState(){
+    this.showDucksData = !this.showDucksData;
+  }
+},
 })
