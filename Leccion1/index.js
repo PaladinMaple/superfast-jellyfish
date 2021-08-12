19acdc279377cc9a842d5d085055553e3f7e5511
Paladin Maple <karina@quadrosoluciones.mx>
2021-08-12 14:59:58 -0500

    Leccion 6 Clases en tiempo real

--- Leccion1/index.js
+++ Leccion1/index.js
@@ -4,12 +4,13 @@ const app = new Vue({
     img: 'https://pbs.twimg.com/profile_images/623323276522188801/D1ehYWlt.jpg',
     name: 'Duck Test',
     //Note: Ducks live between 5 and 10 years
-    duckAge: 12,
+    duckAge: 10,
+    duckMiddleAge: 7,
     ducksNames: ['Mariano', 'Jeremy', 'Juan', 'Carlos', 'Cesar'],
     ducksDetail: [
       { name: 'Mariano', age: 8 },
       { name: 'Jeremy', age: 7 },
-      { name: 'Juan', age: 2 },
+      { name: 'Juan', age: 12 },
       { name: 'Carlos', age: 9 },
       { name: 'Cesar', age: 4 }
   ],
