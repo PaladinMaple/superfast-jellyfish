e43524fa2043c49edc63a90bd4558ae9897b9edd
Paladin Maple <karina@quadrosoluciones.mx>
2021-08-12 09:02:48 -0500

    Leccion 3 Directivas condicionales

--- Leccion2/index.js
+++ Leccion2/index.js
@@ -2,6 +2,8 @@ const app = new Vue({
     el: '#app',    
     data:{
       img: 'https://pbs.twimg.com/profile_images/623323276522188801/D1ehYWlt.jpg',
-      name: 'Duck Test'
+      name: 'Duck Test',
+      duckAge: 12,
+      //Note: Ducks live between 5 and 10 years
     }
 })
