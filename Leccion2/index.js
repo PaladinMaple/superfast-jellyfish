06584c187cef6d2e1d9fe4d627d66b0e83174fb9
Paladin Maple <karina@quadrosoluciones.mx>
2021-08-12 09:29:51 -0500

    Leccion 4 directiva v-for

--- Leccion2/index.js
+++ Leccion2/index.js
@@ -3,7 +3,15 @@ const app = new Vue({
     data:{
       img: 'https://pbs.twimg.com/profile_images/623323276522188801/D1ehYWlt.jpg',
       name: 'Duck Test',
-      duckAge: 12,
       //Note: Ducks live between 5 and 10 years
-    }
+      duckAge: 12,
+      ducksNames: ['Mariano', 'Jeremy', 'Juan', 'Carlos', 'Cesar'],
+      ducksDetail: [
+        { name: 'Mariano', age: 8 },
+        { name: 'Jeremy', age: 7 },
+        { name: 'Juan', age: 2 },
+        { name: 'Carlos', age: 9 },
+        { name: 'Cesar', age: 4 }
+    ]
+  }
 })
